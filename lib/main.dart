55686dad838d44708c412cf15b6a16040717c5ac
karlaujo <karlaujo168@gmail.com>
2022-03-02 02:20:24 -0500

    adding UI for composing messages

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
-
-
 void main() {
   runApp(
     FriendlyChatApp(),
@@ -20,18 +16,60 @@ class FriendlyChatApp extends StatelessWidget {
     return MaterialApp(
       title: 'FriendlyChat',
       home: Chatscreen(),
-      ),
     );
   }
 }
 
-class Chatscreen extends StatelessWidget {
-  const Chatscreen({ Key? key }) : super(key: key);
+class Chatscreen extends StatefulWidget {
+  const Chatscreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<Chatscreen> createState() => _ChatscreenState();
+}
+
+class _ChatscreenState extends State<Chatscreen> {
+  final _textController = TextEditingController();
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Row(
+            children: [
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    const InputDecoration.collapsed(hintText: 'Send a message'),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text)),
+            )
+          ]
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text('FriendlyChat'),
+      ),
+      body: _buildTextComposer(),
     );
   }
 }

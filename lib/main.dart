60f0bf571e12befac232b599c0c44185a2b48877
karlaujo <karlaujo168@gmail.com>
2022-03-12 17:30:06 -0500

    2022-03-12: text messages with name

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+// Copyright 2017, 2020 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    FriendlyChatApp(),
+    const FriendlyChatApp(),
   );
 }
+
+String _name = 'Your Name';
 
 class FriendlyChatApp extends StatelessWidget {
   const FriendlyChatApp({
@@ -13,27 +19,96 @@ class FriendlyChatApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'FriendlyChat',
-      home: Chatscreen(),
+      home: ChatScreen(),
     );
   }
 }
 
-class Chatscreen extends StatefulWidget {
-  const Chatscreen({
+class ChatMessage extends StatelessWidget {
+  const ChatMessage({
+    required this.text,
+    required this.animationController,
+    Key? key,
+  }) : super(key: key);
+  final String text;
+  final AnimationController animationController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0])),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(_name, style: Theme.of(context).textTheme.headline4),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({
     Key? key,
   }) : super(key: key);
 
   @override
-  State<Chatscreen> createState() => _ChatscreenState();
+  State<ChatScreen> createState() => _ChatScreenState();
 }
 
-class _ChatscreenState extends State<Chatscreen> {
+class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin{
+  final List<ChatMessage> _messages = [];
   final _textController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    var message = ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    _focusNode.requestFocus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('FriendlyChat')),
+      body: Column(
+        children: [
+          Flexible(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, index) => _messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+          const Divider(height: 1.0),
+          Container(
+            decoration: BoxDecoration(color: Theme.of(context).cardColor),
+            child: _buildTextComposer(),
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _buildTextComposer() {
@@ -41,14 +116,15 @@ class _ChatscreenState extends State<Chatscreen> {
       data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
       child: Container(
         margin: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Row(
-            children: [
+        child: Row(
+          children: [
             Flexible(
               child: TextField(
                 controller: _textController,
                 onSubmitted: _handleSubmitted,
                 decoration:
                     const InputDecoration.collapsed(hintText: 'Send a message'),
+                focusNode: _focusNode,
               ),
             ),
             Container(
@@ -56,20 +132,10 @@ class _ChatscreenState extends State<Chatscreen> {
               child: IconButton(
                   icon: const Icon(Icons.send),
                   onPressed: () => _handleSubmitted(_textController.text)),
-            )
-          ]
+            ),
+          ],
         ),
       ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('FriendlyChat'),
-      ),
-      body: _buildTextComposer(),
     );
   }
 }
